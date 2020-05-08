c8de1267818d6cc77de5c01e938f53284802160c
Ayush Singh Tomar <AF5E@eurofinsbpt.com>
2020-05-09 04:32:56 +0530

    Inital Release

---
+++ ClashOfClans/Barbarian.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClashOfClans
+{
+    class Barbarian : Troops
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I look like Barbarian.");
+        }
+    }
+}
---
+++ ClashOfClans/Troops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClashOfClans
+{
+    abstract class Troops
+    {
+
+        public int Hitpoints()
+        {
+            return 78;
+        }
+
+        public int Attack()
+        {
+            return 18;
+        }
+
+        public TimeSpan TrainingTime()
+        {
+            return new TimeSpan(0, 0, 5);
+        }
+
+        public abstract void Display();
+
+    }
+}
