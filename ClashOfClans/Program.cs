4227310e5257e3adcaff3516f7578def63b94e11
Ayush Singh Tomar <AF5E@eurofinsbpt.com>
2020-05-09 04:40:22 +0530

    Revert "Inital Release"
    This reverts commit c8de1267818d6cc77de5c01e938f53284802160c.

--- ClashOfClans/Program.cs
+++ ClashOfClans/Program.cs
@@ -1,25 +1,12 @@
 ﻿using System;
 
-namespace ClashOfClans
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // System.out.println("Welcome to Clash of Clans");
-            Console.WriteLine("Welcome to Clash of Clans!\n");
-            Console.WriteLine("--------------------------\n\n");
+namespace ClashOfClans {
+  class Program {
+    static void Main(string[] args) {
 
-            Barbarian b = new Barbarian();
-            b.Display();
-            Console.WriteLine("I am attacking by " + b.Attack() + "%\n\n");
+      // System.out.println("Welcome to Clash of Clans");
+      Console.WriteLine("Welcome to Clash of Clans!");
 
-            Giant g = new Giant();
-            g.Display();
-            Console.WriteLine("I am attacking by " + g.Attack() + "%\n\n");
-
-
-            Console.ReadLine();
-        }
     }
+  }
 }
