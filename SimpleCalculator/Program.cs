e334147c0ea836a81290f2c160d96eecfe661732
Ayush Singh Tomar <AF5E@eurofinsbpt.com>
2020-05-09 18:40:47 +0530

    Added a new solution.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            double num1 = 2;
+            double num2 = 3;
+            double result = 0.0;
+            string choice = "+";
+
+            switch (choice) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine("Result is " + result);
+        }
+    }
+}
