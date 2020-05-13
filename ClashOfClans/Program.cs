9778f0864fd3dfaf027e432415f436f1b32157e3
Ayush Singh Tomar <AF5E@eurofinsbpt.com>
2020-05-14 02:25:28 +0530

    Implemented Final Release

--- ClashOfClans/Program.cs
+++ ClashOfClans/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using ClashOfClans.AbstractClass;
+using ClashOfClans.Implementation.Troop;
 
-namespace ClashOfClans
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // System.out.println("Welcome to Clash of Clans");
-            Console.WriteLine("Welcome to Clash of Clans!\n");
-            Console.WriteLine("--------------------------\n\n");
-        }
+namespace ClashOfClans {
+  class Program {
+    static void Main(string[] args) {
+      // System.out.println("Welcome to Clash of Clans");
+      Console.WriteLine("Welcome to Clash of Clans!\n");
+      Console.WriteLine("--------------------------\n\n");
+
+      Console.WriteLine("Now look at our first Troop");
+      Troops troop = new Giant();
+      troop.Display();
+      troop.TrainingTime();
+      troop.TrainingCost();
+      troop.Attack();
+
     }
+  }
 }
